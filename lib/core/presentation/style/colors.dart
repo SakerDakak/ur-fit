786465b64be15c6a8a6477cadfb62a1ba0de38cb
Saker Dakak <sakeraldakak@gmail.com>
2025-09-14 00:50:58 +0300

    تحسين ألوان التطبيق

--- lib/core/presentation/style/colors.dart
+++ lib/core/presentation/style/colors.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
 
 abstract class Co {
-  static Color notActive = const Color(0xffAAAAAA).withOpacity(0.66);
-  static const Color cardInputStrock = Color(0xffAAAAAA);
+  static Color notActive = const Color(0xffAAAAAA).withValues(alpha: 0.66);
+  static const Color cardInputStrock = Color(0xFF353535);
   static const Color yellow = Color(0xffFFCC00);
-  static const Color strockColor = Color(0xff666666);
+  static const Color strockColor = Color(0xFF303030);
   static const Color selectedFont = Color(0xff1A1A1A);
-  static const Color backGround = Color(0xff333333);
-  static const Color cardColor = Color(0xff414141);
-  static const Color primaryColor = Color(0xff00BDD6);
-  static const Color primaryColorFemale = Color(0xffFA8CA9);
+  static const Color backGround = Color(0xFF191919);
+  static const Color cardColor = Color(0xFF282828);
+  static const Color primaryColor = Color(0xFF00D6E9);
+  static const Color primaryColorFemale = Color(0xFFFF88B2);
   static const Color greyColor = Color(0xffCCCCCC);
-  static const Color darkGrey = Color(0xff999999);
-  static const Color secondaryColor = Color(0xff005A66);
-  // static const Color thirdColor = Color(0xffBAC9FF);
+  static const Color darkGrey = Color(0xFF424242);
+  static const Color secondaryColor = Color(0xFF018293);
   static const Color blackColor = Color(0xff1f2937);
   static const Color whiteColor = Color(0xffFFFFFF);
-  // static const Color greyColor = Color(0xff9CA3AF);
   static const Color menueColor = Color(0xffF3F4F6);
   static const Color redColor = Color(0xffFF0000);
   static const Color yellowColor = Color(0xffFBBF24);
@@ -27,51 +25,6 @@ abstract class Co {
   static const Color purbleColor = Color(0xffA88BFA);
   static const Color babyBlue = Color(0xffEFFBFF);
   static const Color fontColor = Color(0xffF2F2F2);
-  // static const Color darkmodeprimcolor = Color(0xff224791);
-  // static const Color darkmodeseccolor = Color(0xff151a24);
-  // static const Color darkmodescreencolor = Color(0xff0c101b);
-  static Color darkmodewhite90 = const Color(0xffFFFFFF).withOpacity(0.9);
-  static Color darkmodeblack90 = const Color(0xff000000).withOpacity(0.9);
-
-
-
-  // static Color? convertStringToColor(String? color) {
-  //   switch (color) {
-  //     case "green":
-  //       return greenColor;
-  //     case "yellow":
-  //       return yellowColor;
-  //     case "red":
-  //       return redColor;
-  //     case "orange":
-  //       return Colors.orange;
-  //     case "blue":
-  //       return Colors.blueAccent;
-  //     case "gray":
-  //       return Colors.grey;
-  //     default:
-  //       return null;
-  //   }
-  // }
-
-  // static Color? convertStringToColorInTags(String? color) {
-  //   switch (color) {
-  //     case "green":
-  //       return Color(0xff16A34A);
-  //     case "yellow":
-  //       return Color(0xffD97706);
-  //     case "red":
-  //       return Color(0xffEF4444);
-  //     case "orange":
-  //       return Colors.orange;
-  //     case "blue":
-  //       return Colors.blueAccent;
-  //     case "gray":
-  //       return Colors.grey;
-  //     default:
-  //       return null;
-  //   }
-  // }
-
-
+  static Color darkmodewhite90 = const Color(0xffFFFFFF).withValues(alpha: 0.9);
+  static Color darkmodeblack90 = const Color(0xff000000).withValues(alpha: 0.9);
 }
